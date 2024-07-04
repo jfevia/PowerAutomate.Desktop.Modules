6ff21d7b7b1234439f95bda3c29fcce5de379e38
Jesus Fernandez <jfevia@outlook.com>
2024-07-04 23:09:05 +0300

    Localizable resources

--- modules/Modules.WindowsRegistry.Actions/SetRegistryValueAction.cs
+++ modules/Modules.WindowsRegistry.Actions/SetRegistryValueAction.cs
@@ -23,24 +23,27 @@ public class SetRegistryValueAction : ActionBase
     [InputArgument(Order = 1, Required = true)]
     public string Path { get; set; } = null!;
 
-    [InputArgument(Order = 5)]
-    public object ValueBinary { get; set; } = null!;
-
     [InputArgument(Order = 6)]
-    public int ValueInt32 { get; set; }
+    public object Binary { get; set; } = null!;
 
     [InputArgument(Order = 7)]
-    public long ValueInt64 { get; set; }
+    public int DWord { get; set; }
+
+    [InputArgument(Order = 4)]
+    public string ExpandString { get; set; } = null!;
 
     [InputArgument(Order = 0)]
     [DefaultValue(RegistryValueKind.String)]
-    public RegistryValueKind ValueKind { get; set; }
+    public RegistryValueKind Kind { get; set; }
+
+    [InputArgument(Order = 5)]
+    public List<string> MultiString { get; set; } = null!;
+
+    [InputArgument(Order = 8)]
+    public long QWord { get; set; }
 
     [InputArgument(Order = 3)]
-    public string ValueString { get; set; } = null!;
-
-    [InputArgument(Order = 4)]
-    public List<string> ValueStringArray { get; set; } = null!;
+    public string String { get; set; } = null!;
 
     public override void Execute(ActionContext context)
     {
@@ -50,28 +53,30 @@ public class SetRegistryValueAction : ActionBase
         {
             using var registry = new Win32Registry();
             using var registryKey = registry.ParseKey(Path, true);
-            var valueKind = ValueKind.ToAbstractions();
+            var valueKind = Kind.ToAbstractions();
 
-            switch (ValueKind)
+            switch (Kind)
             {
                 case RegistryValueKind.String:
+                    registryKey.SetValue(Name, String, valueKind);
+                    break;
                 case RegistryValueKind.ExpandString:
-                    registryKey.SetValue(Name, ValueString, valueKind);
+                    registryKey.SetValue(Name, ExpandString, valueKind);
                     break;
                 case RegistryValueKind.Binary:
-                    registryKey.SetValue(Name, ValueBinary, valueKind);
+                    registryKey.SetValue(Name, Binary, valueKind);
                     break;
                 case RegistryValueKind.DWord:
-                    registryKey.SetValue(Name, ValueInt32, valueKind);
+                    registryKey.SetValue(Name, DWord, valueKind);
                     break;
                 case RegistryValueKind.MultiString:
-                    registryKey.SetValue(Name, ValueStringArray.ToArray(), valueKind);
+                    registryKey.SetValue(Name, MultiString.ToArray(), valueKind);
                     break;
                 case RegistryValueKind.QWord:
-                    registryKey.SetValue(Name, ValueInt64, valueKind);
+                    registryKey.SetValue(Name, QWord, valueKind);
                     break;
                 default:
-                    throw new NotSupportedException($"Registry value kind '{ValueKind}' is not supported");
+                    throw new NotSupportedException($"Registry value kind '{Kind}' is not supported");
             }
         }
         catch (Exception ex)
@@ -86,13 +91,14 @@ public class SetStringRegistryValueActionSelector : ActionSelector<SetRegistryVa
     public SetStringRegistryValueActionSelector()
     {
         UseName("StringValue");
-        Prop(s => s.ValueKind).ShouldBe(RegistryValueKind.String);
+        Prop(s => s.Kind).ShouldBe(RegistryValueKind.String);
 
         ShowAll();
-        Hide(s => s.ValueStringArray);
-        Hide(s => s.ValueBinary);
-        Hide(s => s.ValueInt32);
-        Hide(s => s.ValueInt64);
+        Hide(s => s.ExpandString);
+        Hide(s => s.MultiString);
+        Hide(s => s.Binary);
+        Hide(s => s.DWord);
+        Hide(s => s.QWord);
     }
 }
 
@@ -101,13 +107,14 @@ public class SetExpandStringRegistryValueActionSelector : ActionSelector<SetRegi
     public SetExpandStringRegistryValueActionSelector()
     {
         UseName("ExpandStringValue");
-        Prop(s => s.ValueKind).ShouldBe(RegistryValueKind.ExpandString);
+        Prop(s => s.Kind).ShouldBe(RegistryValueKind.ExpandString);
 
         ShowAll();
-        Hide(s => s.ValueStringArray);
-        Hide(s => s.ValueBinary);
-        Hide(s => s.ValueInt32);
-        Hide(s => s.ValueInt64);
+        Hide(s => s.String);
+        Hide(s => s.MultiString);
+        Hide(s => s.Binary);
+        Hide(s => s.DWord);
+        Hide(s => s.QWord);
     }
 }
 
@@ -116,13 +123,14 @@ public class SetMultiStringRegistryValueActionSelector : ActionSelector<SetRegis
     public SetMultiStringRegistryValueActionSelector()
     {
         UseName("MultiStringValue");
-        Prop(s => s.ValueKind).ShouldBe(RegistryValueKind.MultiString);
+        Prop(s => s.Kind).ShouldBe(RegistryValueKind.MultiString);
 
         ShowAll();
-        Hide(s => s.ValueString);
-        Hide(s => s.ValueBinary);
-        Hide(s => s.ValueInt32);
-        Hide(s => s.ValueInt64);
+        Hide(s => s.String);
+        Hide(s => s.ExpandString);
+        Hide(s => s.Binary);
+        Hide(s => s.DWord);
+        Hide(s => s.QWord);
     }
 }
 
@@ -131,13 +139,14 @@ public class SetBinaryRegistryValueActionSelector : ActionSelector<SetRegistryVa
     public SetBinaryRegistryValueActionSelector()
     {
         UseName("BinaryValue");
-        Prop(s => s.ValueKind).ShouldBe(RegistryValueKind.Binary);
+        Prop(s => s.Kind).ShouldBe(RegistryValueKind.Binary);
 
         ShowAll();
-        Hide(s => s.ValueString);
-        Hide(s => s.ValueStringArray);
-        Hide(s => s.ValueInt32);
-        Hide(s => s.ValueInt64);
+        Hide(s => s.String);
+        Hide(s => s.ExpandString);
+        Hide(s => s.MultiString);
+        Hide(s => s.DWord);
+        Hide(s => s.QWord);
     }
 }
 
@@ -146,13 +155,14 @@ public class SetInt32RegistryValueActionSelector : ActionSelector<SetRegistryVal
     public SetInt32RegistryValueActionSelector()
     {
         UseName("Int32Value");
-        Prop(s => s.ValueKind).ShouldBe(RegistryValueKind.DWord);
+        Prop(s => s.Kind).ShouldBe(RegistryValueKind.DWord);
 
         ShowAll();
-        Hide(s => s.ValueString);
-        Hide(s => s.ValueStringArray);
-        Hide(s => s.ValueBinary);
-        Hide(s => s.ValueInt64);
+        Hide(s => s.String);
+        Hide(s => s.ExpandString);
+        Hide(s => s.MultiString);
+        Hide(s => s.Binary);
+        Hide(s => s.QWord);
     }
 }
 
@@ -161,12 +171,13 @@ public class SetInt64RegistryValueActionSelector : ActionSelector<SetRegistryVal
     public SetInt64RegistryValueActionSelector()
     {
         UseName("Int64Value");
-        Prop(s => s.ValueKind).ShouldBe(RegistryValueKind.QWord);
+        Prop(s => s.Kind).ShouldBe(RegistryValueKind.QWord);
 
         ShowAll();
-        Hide(s => s.ValueString);
-        Hide(s => s.ValueStringArray);
-        Hide(s => s.ValueBinary);
-        Hide(s => s.ValueInt32);
+        Hide(s => s.String);
+        Hide(s => s.ExpandString);
+        Hide(s => s.MultiString);
+        Hide(s => s.Binary);
+        Hide(s => s.DWord);
     }
 }
