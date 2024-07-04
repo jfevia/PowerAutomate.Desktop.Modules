85d666c67ff45df4edbab1834acbeb0e8a2eaf9b
Jesus Fernandez <jfevia@outlook.com>
2024-07-04 18:22:16 +0300

    Fix certificate import

--- .tools/setup.ps1
+++ .tools/setup.ps1
@@ -1,13 +1,13 @@
 $certAuthority = $env:COMPUTERNAME
 Write-Host "Got computer name"
-$cert = New-SelfSignedCertificate -Type CodeSigningCert -Subject "CN=$certAuthority" -CertStoreLocation "Cert:\CurrentUser\My"
+$cert = New-SelfSignedCertificate -Type CodeSigningCert -Subject "CN=$certAuthority" -CertStoreLocation "Cert:\LocalMachine\My"
 Write-Host "Created self-signed certificate"
 $certPasswordStr = -join ((0..9) + (65..90) + (97..122) | Get-Random -Count 16 | ForEach-Object {[char]$_})
 $certPassword = ConvertTo-SecureString -String $certPasswordStr -Force -AsPlainText
 Write-Host "Created certificate password"
-Export-PfxCertificate -Cert "Cert:\CurrentUser\My\$($cert.Thumbprint)" -FilePath "certificate.pfx" -Password $certPassword
+Export-PfxCertificate -Cert "Cert:\LocalMachine\My\$($cert.Thumbprint)" -FilePath "certificate.pfx" -Password $certPassword
 Write-Host "Exported certificate"
-Import-PfxCertificate -CertStoreLocation "Cert:\CurrentUser\Root" -FilePath "certificate.pfx" -Password $certPassword
+Import-PfxCertificate -CertStoreLocation "Cert:\LocalMachine\Root" -FilePath "certificate.pfx" -Password $certPassword
 Write-Host "Imported certificate"
 Remove-Item "certificate.pfx"
 Write-Host "Deleted certificate"
