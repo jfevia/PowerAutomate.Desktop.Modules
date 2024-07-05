953684384d036a1d260e79497b41406a11c84f0b
Jesus Fernandez <jfevia@outlook.com>
2024-07-05 10:10:13 +0300

    Tests

---
+++ tests/Modules.Windows.Registry.Actions.Tests/ActionSelectorTests.cs
@@ -0,0 +1,35 @@
+﻿// --------------------------------------------------------------
+// Copyright (c) Jesus Fernandez. All Rights Reserved.
+// --------------------------------------------------------------
+
+using System;
+using System.Linq;
+using Microsoft.PowerPlatform.PowerAutomate.Desktop.Actions.SDK.ActionSelectors;
+using NUnit.Framework;
+using PowerAutomate.Desktop.Modules.Windows.Registry.Actions.Properties;
+
+namespace PowerAutomate.Desktop.Modules.Windows.Registry.Actions.Tests;
+
+[TestFixture]
+public class ActionSelectorTests
+{
+    [Test]
+    public void Action_All_HasLocalizableResources()
+    {
+        var resourceManager = Resources.ResourceManager;
+        var resourcesType = typeof(Resources);
+        var assembly = resourcesType.Assembly;
+        var actionSelectorBaseType = typeof(ActionSelectorBase);
+        var actionSelectorTypes = assembly.ExportedTypes
+                                          .Where(type => actionSelectorBaseType.IsAssignableFrom(type))
+                                          .Where(type => type is { IsGenericType: false, IsAbstract: false, IsPublic: true })
+                                          .ToList();
+        var summaryResources = actionSelectorTypes.Select(type => (ActionSelectorBase)Activator.CreateInstance(type))
+                                                  .Select(instance => resourceManager.GetString($"{instance.ActionName}_Summary"));
+
+        foreach (var summaryResource in summaryResources)
+        {
+            Assert.That(summaryResource, Is.Not.Null.Or.Empty);
+        }
+    }
+}
--- tests/Modules.Windows.Registry.Actions.Tests/ActionTests.cs
+++ tests/Modules.Windows.Registry.Actions.Tests/ActionTests.cs
@@ -2,6 +2,7 @@
 // Copyright (c) Jesus Fernandez. All Rights Reserved.
 // --------------------------------------------------------------
 
+using System;
 using System.Linq;
 using System.Reflection;
 using Microsoft.PowerPlatform.PowerAutomate.Desktop.Actions.SDK.Attributes;
@@ -14,6 +15,96 @@ namespace PowerAutomate.Desktop.Modules.Windows.Registry.Actions.Tests;
 public class ActionTests
 {
     [Test]
+    public void Action_All_Arguments_All_Enums_HasLocalizableResources()
+    {
+        var resourceManager = Resources.ResourceManager;
+        var resourcesType = typeof(Resources);
+        var assembly = resourcesType.Assembly;
+        var enumArgumentsByActions = assembly.ExportedTypes
+                                             .Select(type => (ActionType: type, ActionAttribute: type.GetCustomAttribute<ActionAttribute>()))
+                                             .Where(pair => pair.ActionAttribute is not null)
+                                             .Select(pair => (
+                                                 pair.ActionType,
+                                                 pair.ActionAttribute,
+                                                 Arguments: pair.ActionType
+                                                                .GetProperties(BindingFlags.Public | BindingFlags.Instance)
+                                                                .Select(property => (Property: property, InputArgumentAttribute: property.GetCustomAttribute<InputArgumentAttribute>(), OutputArgumentAttribute: property.GetCustomAttribute<OutputArgumentAttribute>()))
+                                                                .Where(property => property.InputArgumentAttribute is not null || property.OutputArgumentAttribute is not null)
+                                                                .Where(property => property.Property.PropertyType.IsEnum)
+                                                                .ToList())
+                                             )
+                                             .Where(pair => pair.Arguments.Any())
+                                             .ToList();
+        var enumValuesByPropertyTypes = enumArgumentsByActions.SelectMany(s => s.Arguments.Select(argument => argument.Property.PropertyType))
+                                                              .Distinct()
+                                                              .Select(propertyType => (PropertyType: propertyType, Values: Enum.GetValues(propertyType)
+                                                                                                                               .OfType<Enum>()
+                                                                                                                               .ToList()))
+                                                              .ToList();
+        foreach (var friendlyNameResource in enumValuesByPropertyTypes.SelectMany(s => s.Values.Select(value => resourceManager.GetString($"{s.PropertyType.Name}_{value}_FriendlyName"))))
+        {
+            Assert.That(friendlyNameResource, Is.Not.Null.Or.Empty);
+        }
+    }
+
+    [Test]
+    public void Action_All_Arguments_All_HasLocalizableResources()
+    {
+        var resourceManager = Resources.ResourceManager;
+        var resourcesType = typeof(Resources);
+        var assembly = resourcesType.Assembly;
+        var argumentsByActions = assembly.ExportedTypes
+                                         .Select(type => (ActionType: type, ActionAttribute: type.GetCustomAttribute<ActionAttribute>()))
+                                         .Where(pair => pair.ActionAttribute is not null)
+                                         .Select(pair => (
+                                             pair.ActionType,
+                                             pair.ActionAttribute,
+                                             Arguments: pair.ActionType
+                                                            .GetProperties(BindingFlags.Public | BindingFlags.Instance)
+                                                            .Select(property => (Property: property, InputArgumentAttribute: property.GetCustomAttribute<InputArgumentAttribute>(), OutputArgumentAttribute: property.GetCustomAttribute<OutputArgumentAttribute>()))
+                                                            .Where(property => property.InputArgumentAttribute is not null || property.OutputArgumentAttribute is not null)
+                                                            .ToList())
+                                         )
+                                         .ToList();
+
+        foreach (var argumentsByAction in argumentsByActions)
+        {
+            foreach (var argument in argumentsByAction.Arguments)
+            {
+                var friendlyNameResource = resourceManager.GetString($"{argumentsByAction.ActionAttribute.Id}_{argument.Property.Name}_FriendlyName");
+                var descriptionResource = resourceManager.GetString($"{argumentsByAction.ActionAttribute.Id}_{argument.Property.Name}_Description");
+
+                Assert.That(friendlyNameResource, Is.Not.Null.Or.Empty);
+                Assert.That(descriptionResource, Is.Not.Null.Or.Empty);
+            }
+        }
+    }
+
+    [Test]
+    public void Action_All_Errors_All_HasLocalizableResources()
+    {
+        var resourceManager = Resources.ResourceManager;
+        var resourcesType = typeof(Resources);
+        var assembly = resourcesType.Assembly;
+        var errors = assembly.ExportedTypes
+                             .Select(type => (ActionType: type, ActionAttribute: type.GetCustomAttribute<ActionAttribute>()))
+                             .Where(pair => pair.ActionAttribute is not null)
+                             .SelectMany(pair => pair.ActionType.GetCustomAttributes<ThrowsAttribute>())
+                             .Select(attribute => attribute.Name)
+                             .Distinct()
+                             .ToList();
+
+        foreach (var error in errors)
+        {
+            var friendlyNameResource = resourceManager.GetString($"Error_{error}_FriendlyName");
+            var descriptionResource = resourceManager.GetString($"Error_{error}_Description");
+
+            Assert.That(friendlyNameResource, Is.Not.Null.Or.Empty);
+            Assert.That(descriptionResource, Is.Not.Null.Or.Empty);
+        }
+    }
+
+    [Test]
     public void Action_All_HasLocalizableResources()
     {
         var resourceManager = Resources.ResourceManager;
@@ -21,7 +112,8 @@ public class ActionTests
         var assembly = resourcesType.Assembly;
         var actions = assembly.ExportedTypes
                               .Select(type => (ActionType: type, ActionAttribute: type.GetCustomAttribute<ActionAttribute>()))
-                              .Where(pair => pair.ActionAttribute is not null);
+                              .Where(pair => pair.ActionAttribute is not null)
+                              .ToList();
 
         foreach (var action in actions)
         {
