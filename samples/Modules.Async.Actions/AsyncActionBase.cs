9d9885acfa926207a2895b29ac22e37ba3e1f1b0
Jesus Fernandez <jfevia@outlook.com>
2024-07-11 22:37:43 +0300

    Initial implementation

---
+++ samples/Modules.Async.Actions/AsyncActionBase.cs
@@ -0,0 +1,27 @@
+﻿// --------------------------------------------------------------
+// Copyright (c) Jesus Fernandez. All Rights Reserved.
+// --------------------------------------------------------------
+
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.PowerPlatform.PowerAutomate.Desktop.Actions.SDK;
+using PowerAutomate.Desktop.Modules.Actions.Shared;
+
+namespace PowerAutomate.Desktop.Modules.Async.Actions;
+
+public abstract class AsyncActionBase : ActionBase
+{
+    protected abstract Task ExecuteAsync(ActionContext context, CancellationToken cancellationToken);
+
+    public override void Execute(ActionContext context)
+    {
+        Debugger.Launch();
+        AsyncScheduler.Default.Queue(InternalExecute);
+        return;
+
+        async Task InternalExecute(CancellationToken cancellationToken)
+        {
+            await ExecuteAsync(context, cancellationToken);
+        }
+    }
+}
---
+++ samples/Modules.Async.Actions/LongRunningAction.cs
@@ -0,0 +1,44 @@
+﻿// --------------------------------------------------------------
+// Copyright (c) Jesus Fernandez. All Rights Reserved.
+// --------------------------------------------------------------
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.PowerPlatform.PowerAutomate.Desktop.Actions.SDK;
+using Microsoft.PowerPlatform.PowerAutomate.Desktop.Actions.SDK.Attributes;
+using PowerAutomate.Desktop.Modules.Actions.Shared;
+
+namespace PowerAutomate.Desktop.Modules.Async.Actions;
+
+[Action(Id = "LongRunning")]
+[Throws(ErrorCodes.Unknown)]
+[SuppressMessage("ReSharper", "AutoPropertyCanBeMadeGetOnly.Global", Justification = "PowerAutomate.Desktop.Module.Action")]
+[SuppressMessage("ReSharper", "MemberCanBePrivate.Global", Justification = "PowerAutomate.Desktop.Module.Action")]
+[SuppressMessage("ReSharper", "UnusedAutoPropertyAccessor.Global", Justification = "PowerAutomate.Desktop.Module.Action")]
+[SuppressMessage("ReSharper", "ClassNeverInstantiated.Global", Justification = "PowerAutomate.Desktop.Module.Action")]
+[SuppressMessage("ReSharper", "UnusedType.Global", Justification = "PowerAutomate.Desktop.Module.Action")]
+public class LongRunningAction : AsyncActionBase
+{
+    [InputArgument(Order = 2)]
+    public int Maximum { get; set; }
+
+    [InputArgument(Order = 1)]
+    public int Minimum { get; set; }
+
+    protected override async Task ExecuteAsync(ActionContext context, CancellationToken cancellationToken)
+    {
+        Debugger.Launch();
+
+        try
+        {
+            var delayInMilliseconds = RandomHelper.Shared.Next(Minimum, Maximum);
+            await Task.Delay(delayInMilliseconds, cancellationToken);
+        }
+        catch (Exception ex)
+        {
+            throw new ActionException(ErrorCodes.Unknown, ex.Message, ex);
+        }
+    }
+}
