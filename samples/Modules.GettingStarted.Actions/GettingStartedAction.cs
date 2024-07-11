2f6a6a0372da63741a530d9bfc0895e74437b8a8
Jesus Fernandez <jfevia@outlook.com>
2024-07-11 14:40:17 +0300

    Initial implementation

---
+++ samples/Modules.GettingStarted.Actions/GettingStartedAction.cs
@@ -0,0 +1,23 @@
+﻿// --------------------------------------------------------------
+// Copyright (c) Jesus Fernandez. All Rights Reserved.
+// --------------------------------------------------------------
+
+using Microsoft.PowerPlatform.PowerAutomate.Desktop.Actions.SDK;
+using Microsoft.PowerPlatform.PowerAutomate.Desktop.Actions.SDK.Attributes;
+
+namespace PowerAutomate.Desktop.Modules.GettingStarted.Actions;
+
+[Action]
+public class GettingStartedAction : ActionBase
+{
+    [OutputArgument]
+    public string Message { get; set; } = null!;
+
+    [InputArgument]
+    public string Name { get; set; } = null!;
+
+    public override void Execute(ActionContext context)
+    {
+        Message = $"Hello, {Name}!";
+    }
+}
