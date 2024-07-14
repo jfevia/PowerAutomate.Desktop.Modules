8065e3fa6035b641b4f7e141a36adf880db5cfc9
Jesus Fernandez <jfevia@outlook.com>
2024-07-14 14:20:37 +0300

    Initial implementation

---
+++ plugins/Plugins.SoftDelete/SoftDeletePlugin.cs
@@ -0,0 +1,17 @@
+﻿// ---------------------------------------------------
+// Copyright (c) Jesus Fernandez. All Rights Reserved.
+// ---------------------------------------------------
+
+using System;
+using Microsoft.Extensions.DependencyInjection;
+using PowerAutomate.Desktop.Plugins.Shared;
+
+namespace PowerAutomate.Desktop.Plugins.SoftDelete;
+
+public sealed class SoftDeletePlugin : Plugin
+{
+    public override void ConfigureServices(IServiceCollection services)
+    {
+        throw new NotImplementedException();
+    }
+}
