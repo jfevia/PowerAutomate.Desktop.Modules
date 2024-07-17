cff0bca816d56754e80130d05f86919f7b65c232
Jesus Fernandez <jfevia@outlook.com>
2024-07-17 21:28:11 +0300

    Initial implementation

---
+++ modules/Modules.AzureKeyVault.Actions/ErrorCodes.cs
@@ -0,0 +1,10 @@
+﻿// ---------------------------------------------------
+// Copyright (c) Jesus Fernandez. All Rights Reserved.
+// ---------------------------------------------------
+
+namespace PowerAutomate.Desktop.Modules.AzureKeyVault.Actions;
+
+internal static class ErrorCodes
+{
+    public const string Unknown = "UnknownError";
+}
