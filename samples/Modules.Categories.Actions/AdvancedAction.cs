a99ce33b1c921ce6933e290c1ef0448c5ff1bfa8
Jesus Fernandez <jfevia@outlook.com>
2024-08-04 07:38:45 +0300

    Sample: Action categories (#21)

---
+++ samples/Modules.Categories.Actions/AdvancedAction.cs
@@ -0,0 +1,26 @@
+﻿// ---------------------------------------------------
+// Copyright (c) Jesus Fernandez. All Rights Reserved.
+// ---------------------------------------------------
+
+using Microsoft.PowerPlatform.PowerAutomate.Desktop.Actions.SDK;
+using Microsoft.PowerPlatform.PowerAutomate.Desktop.Actions.SDK.Attributes;
+
+namespace PowerAutomate.Desktop.Modules.Categories.Actions;
+
+[Action]
+public class RootAction : ActionBase
+{
+    public override void Execute(ActionContext context)
+    {
+        // Nothing
+    }
+}
+
+[Action(Category = "Advanced")]
+public class AdvancedAction : ActionBase
+{
+    public override void Execute(ActionContext context)
+    {
+        // Nothing
+    }
+}
