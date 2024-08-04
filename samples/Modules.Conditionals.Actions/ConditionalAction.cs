ac374b6b43d0c50354aaf9d5e2ed820dbd49c1b9
Jesus Fernandez <jfevia@outlook.com>
2024-08-04 07:50:11 +0300

    Initial implementation

---
+++ samples/Modules.Conditionals.Actions/ConditionalAction.cs
@@ -0,0 +1,19 @@
+﻿// ---------------------------------------------------
+// Copyright (c) Jesus Fernandez. All Rights Reserved.
+// ---------------------------------------------------
+
+using Microsoft.PowerPlatform.PowerAutomate.Desktop.Actions.SDK;
+using Microsoft.PowerPlatform.PowerAutomate.Desktop.Actions.SDK.Attributes;
+
+namespace PowerAutomate.Desktop.Modules.Conditionals.Actions;
+
+[ConditionAction(ResultPropertyName = nameof(Result))]
+public class ConditionalAction : ActionBase
+{
+    public bool Result { get; set; }
+
+    public override void Execute(ActionContext context)
+    {
+        // Nothing
+    }
+}
