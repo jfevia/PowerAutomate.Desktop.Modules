9d23cb27e32e9c8f35b1100ac66deb448a60d568
Jesus Fernandez <jfevia@outlook.com>
2024-08-04 08:08:54 +0300

    Initial implementation

---
+++ modules/Modules.PowerFx.Actions/ExecutePowerFxExpressionAction.cs
@@ -0,0 +1,48 @@
+﻿// ---------------------------------------------------
+// Copyright (c) Jesus Fernandez. All Rights Reserved.
+// ---------------------------------------------------
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using Microsoft.PowerFx;
+using Microsoft.PowerPlatform.PowerAutomate.Desktop.Actions.SDK;
+using Microsoft.PowerPlatform.PowerAutomate.Desktop.Actions.SDK.Attributes;
+
+namespace PowerAutomate.Desktop.Modules.PowerFx.Actions;
+
+[Action(Id = "ExecutePowerFxExpression")]
+[Throws(ErrorCodes.Unknown)]
+[SuppressMessage("ReSharper", "AutoPropertyCanBeMadeGetOnly.Global", Justification = "PowerAutomate.Desktop.Module.Action")]
+[SuppressMessage("ReSharper", "MemberCanBePrivate.Global", Justification = "PowerAutomate.Desktop.Module.Action")]
+[SuppressMessage("ReSharper", "UnusedAutoPropertyAccessor.Global", Justification = "PowerAutomate.Desktop.Module.Action")]
+[SuppressMessage("ReSharper", "ClassNeverInstantiated.Global", Justification = "PowerAutomate.Desktop.Module.Action")]
+[SuppressMessage("ReSharper", "UnusedType.Global", Justification = "PowerAutomate.Desktop.Module.Action")]
+public class ExecutePowerFxExpressionAction : ActionBase
+{
+    [InputArgument]
+    public string Expression { get; set; } = null!;
+
+    [OutputArgument]
+    public string Result { get; set; } = null!;
+
+    public override void Execute(ActionContext context)
+    {
+        try
+        {
+            var engine = new Engine();
+            var result = engine.Parse(Expression);
+
+            if (!result.IsSuccess)
+            {
+                throw new AggregateException(result.Errors.Select(error => new Exception(error.Message)));
+            }
+
+            Result = result.Text;
+        }
+        catch (Exception ex)
+        {
+            throw new ActionException(ErrorCodes.Unknown, ex.Message, ex);
+        }
+    }
+}
